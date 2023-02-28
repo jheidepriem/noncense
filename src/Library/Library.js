f6b7a54c92a5882c2d8288dbb539ad27b636977a
Josephine Heidepriem <jheidepriem@gmail.com>
2023-02-28 17:02:26 -0600

    Add Library architecture

--- src/Library/Library.js
+++ src/Library/Library.js
@@ -1,8 +1,24 @@
 import React from 'react';
-import './Library.css'
+import Book from '../Book/Book';
+import './Library.css';
 
 const Library = ({ allBooks }) => {
+  const mappedBooks = allBooks.map((book) => {
+    return (
+      <div key={book.key}>
+      <Book
+        id={book.key}
+        cover_id={book.cover_id}
+      />
+      </div>
+    )
+  })
   
+  return (
+    <section className='books-container'>
+      {mappedBooks}
+    </section>
+  )
 }
 
 export default Library
