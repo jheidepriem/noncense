b934040823f2679549ca8aca2b6dcffe47e4736d
Josephine Heidepriem <jheidepriem@gmail.com>
2023-02-28 16:20:48 -0600

    Add Book data

--- src/Book/Book.js
+++ src/Book/Book.js
@@ -1,6 +1,16 @@
 import React from 'react'
 import './Book.css'
 
-
+const Book = ({ key, cover_id }) => {
+  return (
+    <section className='book'>
+      <img 
+      className="book-cover"
+      src={`https://covers.openlibrary.org/b/id/${cover_id}-M.jpg`}
+      alt="book cover"
+      />
+  </section>
+  )
+}
 
 export default Book
