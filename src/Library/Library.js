c30d83c88d46b956ee2ded95b4e3844acb2940ca
Josephine Heidepriem <jheidepriem@gmail.com>
2023-03-01 14:36:17 -0600

    Change book id data

--- src/Library/Library.js
+++ src/Library/Library.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import Book from '../Book/Book';
-import './Library.css';
+import Book from "../Book/Book";
+import "./Library.css";
 
 const Library = ({ allBooks }) => {
   const mappedBooks = allBooks.map((book) => {
     return (
       <div key={book.key}>
-      <Book
-        id={book.key}
-        cover_id={book.cover_id}
-      />
+        <Book id={book.ia} cover_id={book.cover_id} />
       </div>
-    )
-  })
-  
-  return (
-    <section className='books-container'>
-      {mappedBooks}
-    </section>
-  )
-}
+    );
+  });
 
-export default Library
+  return <section className="books-container">{mappedBooks}</section>;
+};
+
+export default Library;
