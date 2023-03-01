14f19ac8431be115ac256daefe7a04b68abcff39
Josephine Heidepriem <jheidepriem@gmail.com>
2023-03-01 14:35:30 -0600

    Replace navlink with link

--- src/Book/Book.js
+++ src/Book/Book.js
@@ -1,18 +1,18 @@
-import React, { NavLink } from 'react'
-import './Book.css'
+import { Link } from "react-router-dom";
+import "./Book.css";
 
 const Book = ({ cover_id, id }) => {
   return (
-    <NavLink to={`/${id}`} key={id}>
-    <section className='book'>
-      <img 
-      className="book-cover"
-      src={`https://covers.openlibrary.org/b/id/${cover_id}-M.jpg`}
-      alt="book cover"
-      />
-  </section>
-  </NavLink>
-  )
-}
+    <Link to={`/book/${id}`} key={id}>
+      <section className="book">
+        <img
+          className="book-cover"
+          src={`https://covers.openlibrary.org/b/id/${cover_id}-M.jpg`}
+          alt="book cover"
+        />
+      </section>
+    </Link>
+  );
+};
 
-export default Book
+export default Book;
