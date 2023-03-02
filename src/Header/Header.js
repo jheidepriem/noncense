af554076ffe4b7d186bf1e088926ec89b7a0dc3c
Josephine Heidepriem <jheidepriem@gmail.com>
2023-03-01 21:44:13 -0600

    Add header text

---
+++ src/Header/Header.js
@@ -0,0 +1,16 @@
+
+import '../Header/Header.css'
+
+const Header = () => {
+return (
+    <header className="header-bar">
+      <section className="title-logo">
+        {/* <img className="logo-image" src="filmLogo.png" alt="filmLogo" /> */}
+        <h1 className="header-title">Non.Cense</h1>
+      </section>
+  
+    </header>
+  );
+}
+
+export default Header;
