1530a8fceb9a5156cde11922b2cd896c29575a88
Josephine Heidepriem <jheidepriem@gmail.com>
2023-03-02 12:36:06 -0600

    Add search bar in form

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,32 +1,24 @@
-import { useState } from "react";
-import "../Form/Form.css";
+import { useState, useEffect } from 'react';
+import '../Form/Form.css'
 
-const Form = ({ getSearch }) => {
-  const [search, setSearch] = useState("");
-  const [error, setError] = useState(false);
+const Form = ({ filterBooks }) => {
+  const [search, setSearch] = useState('')
 
-  const submitSearch = (e) => {
-    e.preventDefault();
-    if (!searchInput) {
-      setError(true);
-    } else {
-      setError(false);
-      getSearch(searchInput);
-    }
-    setSearch("");
-  };
+useEffect(() => {
+  filterBooks(search)
+}, [search])
 
-  return (
-    <form>
-      <input
-        type="text"
-        placeholder="Search"
-        name="search"
-        value={search}
-        onChange={(event) => setSearch(event.target.value)}
-      />
-    </form>
-  );
-};
+return (
+  <form className ="search-form">
+    <input
+    type='text'
+    placeholder='Search'
+    name='search'
+    value={search}
+    onChange={event => setSearch(event.target.value)}
+  />
+  </form>
+ )
+}
 
 export default Form;
