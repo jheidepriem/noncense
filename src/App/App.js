85ab50d3e909595351aa7184e2c2d55231f8d738
Josephine Heidepriem <jheidepriem@gmail.com>
2023-03-01 21:41:11 -0600

    Add spacing

--- src/App/App.js
+++ src/App/App.js
@@ -3,6 +3,7 @@ import { Route, Switch } from "react-router-dom";
 import { fetchData } from "../Api";
 import Library from "../Library/Library";
 import BookDetails from "../BookDetails/BookDetails";
+import Header from "../Header/Header";
 import "../App/App.css";
 
 const App = () => {
@@ -14,7 +15,7 @@ const App = () => {
     setLoading(true);
     fetchData()
       .then((data) => {
-       setLibraryData(data.works);
+        setLibraryData(data.works);
         setLoading(false);
       })
       .catch((error) => console.log(error, "Error fetching library"));
@@ -24,11 +25,11 @@ const App = () => {
     <main className="App">
       <Switch>
         <Route
-         exact
+          exact
           path="/"
           render={() => (
             <Fragment>
-              <h1>Noncense</h1>
+              <Header />
               {/* <Form/> */}
               <Library allBooks={libraryData} />
               {Loading && <h1>Loading...</h1>}
@@ -36,27 +37,18 @@ const App = () => {
           )}
         />
         <Route
-         exact
+          exact
           path="/book/:id"
           render={({ match }) => {
-            const findBook = libraryData.find((book) => book.ia === match.params.id)
-            return <BookDetails bookData={findBook} id={match.params.id}/>
+            const findBook = libraryData.find(
+              (book) => book.ia === match.params.id
+            );
+            return <BookDetails bookData={findBook} id={match.params.id} />;
           }}
-       />
+        />
       </Switch>
     </main>
   );
 };
 
 export default App;
-
-// Each Book [ {
-
-//   key: ‘/works/OL52922A’
-//   title: ‘The Handmaid’s Tale’
-//   author: [ {name: ‘Margaret Atwood’} ]
-//   cover_id: 8231851. Will have to be interpolated with url https://covers.openlibrary.org/b/id/8231851-M.jpg
-//   availability: {available_to_borrow: true}
-//   first_publish_year: 1985
-
-//   ]
