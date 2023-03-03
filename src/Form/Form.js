39c60055db847df6f5d28d038c2e794f2252a732
Josephine Heidepriem <jheidepriem@gmail.com>
2023-03-02 20:09:36 -0600

    Refactor form css search bar

--- src/Form/Form.js
+++ src/Form/Form.js
@@ -1,24 +1,27 @@
-import { useState, useEffect } from 'react';
-import '../Form/Form.css'
+import { useState, useEffect } from "react";
+import "../Form/Form.css";
 
 const Form = ({ filterBooks }) => {
-  const [search, setSearch] = useState('')
+  const [search, setSearch] = useState("");
 
-useEffect(() => {
-  filterBooks(search)
-}, [search])
+  useEffect(() => {
+    filterBooks(search);
+  }, [search]);
 
-return (
-  <form className ="search-form">
-    <input
-    type='text'
-    placeholder='Search'
-    name='search'
-    value={search}
-    onChange={event => setSearch(event.target.value)}
-  />
-  </form>
- )
-}
+  return (
+    <section className="search-form-container">
+        <form className="search-form">
+          <input
+            type="text"
+            placeholder="Search for books"
+            name="search"
+            value={search}
+            onChange={(event) => setSearch(event.target.value)}
+          />
+        </form>
+      </section>
+   
+  );
+};
 
 export default Form;
