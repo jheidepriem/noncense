dc4350768fd23bb8db4a7bfb4f15afc82e66f663
Josephine Heidepriem <jheidepriem@gmail.com>
2023-03-04 14:38:52 -0600

    Add a few more tests to app e2e

--- cypress/e2e/App.cy.js
+++ cypress/e2e/App.cy.js
@@ -1,23 +1,29 @@
-describe('about page', () => {
+describe("about page", () => {
   beforeEach(() => {
-    cy.intercept('GET', "https://openlibrary.org/subjects/collectionid:bannedbooks.json?limit=100", {fixture: "books.json"})
-    cy.visit("http://localhost:3000")
-  })
-  
-  it('should show all of the elements on the main about page', () => {
-    cy.get('.logo').should('be.visible')
-    cy.get('.welcome-title').should('be.visible')
-    cy.get('.about-text').should('be.visible')
-    cy.get('.map-image').should('be.visible')
-    cy.get('.about-text-2').should('be.visible')
-    cy.get('.button').should('be.visible')
-  })
+    cy.intercept(
+      "GET",
+      "https://openlibrary.org/subjects/collectionid:bannedbooks.json?limit=100",
+      { fixture: "books.json" }
+    );
+    cy.visit("http://localhost:3000");
+  });
 
-  it('should navigate to the library on button click', () => {
-    cy.get('button').click()
-    cy.url('http://localhost:3000/library')
-    
-  })
+  it("should show all of the elements on the main about page", () => {
+    cy.get(".logo").should("be.visible");
+    cy.get(".welcome-title").should("be.visible");
+    cy.get(".about-text").should("be.visible");
+    cy.get(".map-image").should("be.visible");
+    cy.get(".about-text-2").should("be.visible");
+    cy.get(".button").should("be.visible");
+  });
 
-
-})
+  it("should navigate to the library on button click", () => {
+    cy.get("button").click();
+    cy.url("http://localhost:3000/library");
+    cy.get(".logo").should("be.visible");
+    cy.get("input").should("be.visible");
+    cy.get(".books-container").should("be.visible");
+    cy.get(":nth-child(1) > a > .book > .book-cover").should("be.visible");
+    cy.get(":nth-child(2) > a > .book > .book-cover").should("be.visible");
+  });
+});
