d893fda0ce202f571e5515a71acde418f76dd880
Josephine Heidepriem <jheidepriem@gmail.com>
2023-03-04 11:13:51 -0600

    Add cypress tests for Main page

--- cypress/e2e/App.cy.js
+++ cypress/e2e/App.cy.js
@@ -3,6 +3,7 @@ describe('about page', () => {
     cy.intercept('GET', "https://openlibrary.org/subjects/collectionid:bannedbooks.json?limit=100", {fixture: "books.json"})
     cy.visit("http://localhost:3000")
   })
+  
   it('should show all of the elements on the main about page', () => {
     cy.get('.logo').should('be.visible')
     cy.get('.welcome-title').should('be.visible')
@@ -11,4 +12,10 @@ describe('about page', () => {
     cy.get('.about-text-2').should('be.visible')
     cy.get('.button').should('be.visible')
   })
+
+  it('should navigate to the library on button click', () => {
+    cy.get('button').click()
+    cy.url('http://localhost:3000/library')
+  })
+
 })
