36f51186c4c0769074c1299e71355ee8fb7c76e3
Josephine Heidepriem <jheidepriem@gmail.com>
2023-03-04 09:53:02 -0600

    Add cypress

---
+++ cypress/e2e/App.cy.js
@@ -0,0 +1,14 @@
+describe('about page', () => {
+  beforeEach(() => {
+    cy.intercept('GET', "https://openlibrary.org/subjects/collectionid:bannedbooks.json?limit=100", {fixture: "books.json"})
+    cy.visit("http://localhost:3000")
+  })
+  it('should show all of the elements on the main about page', () => {
+    cy.get('.logo').should('be.visible')
+    cy.get('.welcome-title').should('be.visible')
+    cy.get('.about-text').should('be.visible')
+    cy.get('.map-image').should('be.visible')
+    cy.get('.about-text-2').should('be.visible')
+    cy.get('.button').should('be.visible')
+  })
+})
